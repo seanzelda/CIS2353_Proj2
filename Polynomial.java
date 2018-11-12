a0319d61620940b390a76131781744fddb178a50
seanzelda <szelda@umich.edu>
2018-11-12 09:42:29 -0500

    Add files via upload

---
+++ Polynomial.java
@@ -0,0 +1,124 @@
+public class Polynomial {
+    private static Node firstNode;
+    private int numberOfNodes;
+    
+    public Polynomial() {
+        firstNode = null;
+        numberOfNodes = 0;
+    }
+    
+    public Polynomial(String poly) {
+        String[] polyArray = parsePolyString(poly);
+        for(int i = 0; i < polyArray.length; i++) {
+            Node node = getNode(polyArray[i]);
+            if(isEmpty()) {
+                firstNode = node;
+                numberOfNodes++;
+            }
+            else {
+                Node walker = firstNode;
+                while(walker.nextNode != null) {
+                    walker = walker.nextNode;
+                }
+                walker.nextNode = node;
+                numberOfNodes++;
+            }
+        }
+
+
+        
+    }
+    
+    public Polynomial(Polynomial otherPoly) {
+    
+//        for(int i = 0; i < otherPoly.numberOfNodes; i++) {
+//            if(isEmpty()) {
+//                firstNode = otherPoly.firstNode;
+//                numberOfNodes++;
+//            }
+//            else {
+//                Node walker = otherPoly.firstNode;
+//                Node newWalker = firstNode;
+//                while(walker.nextNode != null) {
+//                    walker = walker.nextNode;
+//                    newWalker = newWalker.nextNode;
+//                }
+//                newWalker.nextNode = walker;
+//                numberOfNodes++;
+//            }
+//        }
+    }
+    
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0; i < numberOfNodes; i++) {
+            Node walker = firstNode;
+                while(walker.nextNode != null) {
+                    walker = walker.nextNode;
+                }  
+               
+        }
+        return builder.toString();
+    }
+    
+    public String print() {
+        return this.toString();
+    }
+    
+    //public static boolean add(Polynomial poly1, Polynomial poly2) {
+        //Node newNode = new Node();
+        
+        //newNode.nextNode = firstNode;
+        
+        //firstNode = newNode;
+        //numberOfNodes++;
+        
+        //return true;     
+    //}
+    
+    public boolean isEmpty() {
+        return firstNode == null;
+    }
+
+    private Node getNode(String monomial) {
+
+            String[] monArray = monomial.split("x");
+            try {
+                 monArray[1] = monArray[1].replace("^", "");
+            }
+            catch (Exception ex) {
+                
+            }
+            if(monArray.length == 1) {
+                return new Node(Integer.parseInt(monArray[0]), -1);
+            }
+            return new Node(Integer.parseInt(monArray[0]), Integer.parseInt(monArray[1]));
+    }
+    
+    private String[] parsePolyString(String poly) {
+        String[] polyArray = poly.split("\\+");
+        for(int i = 0; i < polyArray.length; i++) {
+            polyArray[i] = polyArray[i].replace("-", "+-");
+        }
+        return polyArray;
+    }
+    
+    private static class Node {
+        private int coefficient;
+        private int exponent;
+        private Node nextNode;
+    
+        private Node(int coefficient, int exponent) {
+            this.coefficient = coefficient;
+            this.exponent = exponent;
+            nextNode = null;
+        }
+        
+        private Node(int coefficient, int exponent, Node nextNode) {
+            this.coefficient = coefficient;
+            this.exponent = exponent;
+            this.nextNode = nextNode;
+        }
+}
+
+}
---
+++ PolynomialDemo.java
@@ -0,0 +1,85 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PolynomialDemo {
+    public static void main(String[] args) {
+        ArrayList<Polynomial> myArrayList = getPolyList();
+
+        
+        boolean keepGoing = true;
+        Scanner input = new Scanner(System.in);
+        String inputString;
+        ArrayList<Integer> addNumbers = new ArrayList<>();
+        while (keepGoing) {
+            for(int i = 0; i < myArrayList.size(); i++) {
+                System.out.println(i + " : " + myArrayList.get(i));
+            }//end for
+            
+            System.out.println("Which do you wish to add? Press -1 to Exit.");
+            inputString = input.nextLine();
+            
+            String[] inputArray = inputString.split(" ");
+            boolean goodInput = checkNumberRange(inputArray, myArrayList);
+            if (!goodInput) {
+                if(Integer.parseInt(inputArray[0]) == -1) {
+                    keepGoing = false;
+            }
+            else {
+                System.out.println("Sorry, that input is invalid.");
+            }
+            if (Integer.parseInt(inputArray[0]) == -1) {
+                keepGoing = false;
+            }
+            else {
+                for(String out : inputArray) {
+                    addNumbers.add(Integer.parseInt(out));                  
+                }
+                //Remove after testing
+                
+                for(int out : addNumbers) {
+                    System.out.println(out);
+                }
+
+            }
+        }
+        }
+    }
+    private static ArrayList<Polynomial> getPolyList() {
+        ArrayList<Polynomial> myArrayList = new ArrayList<>();
+        
+        try {
+            File file = new File("/Users/seanzelda/Java/workdr/Node/src/polylist.txt");
+            Scanner infile = new Scanner(file);
+            infile = new Scanner(file);
+            
+            while(infile.hasNext()) {
+                Polynomial poly = 
+                        new Polynomial(infile.nextLine());
+                myArrayList.add(poly);
+                System.out.println(poly);
+            }//end while
+            infile.close();
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println("Woops. File not found!");
+        }//end try-catch
+        
+        return myArrayList;
+    }
+    
+    private static boolean checkNumberRange(String[] addNumbers, ArrayList<Polynomial> polys) {
+        for(String index : addNumbers) {
+            try {
+                polys.get(Integer.parseInt(index));
+            }
+            catch (IndexOutOfBoundsException ex) {
+
+                return false;
+            }
+        }
+        return true;
+        
+    }
+    } 
